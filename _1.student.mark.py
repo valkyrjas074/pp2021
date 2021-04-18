d8c99cae980abd2efd67dad14ce097c5fdc83b56
Tran <dungt.bi9074@st.usth.edu.vn>
2021-04-19 00:52:00 +0700

    Practical work 2

---
+++ _1.student.mark.py
@@ -0,0 +1,10 @@
+snum = input("Input the number of students:")
+for num in range(snum):
+id = input("Input the student id:")
+name = input("Input the student name:")
+DoB = input("Input the student date of birth:")
+cnum = input("Input the number of courses:")
+for num in range(cnum)
+cid = input("Input the course id:")
+cname = input("Input the course name:")
+for 
